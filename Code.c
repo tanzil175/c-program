f03dc43a5de0f1e27e315f550ed74475e7d6b6cf
unknown <tanzilmallick15@gmail.com>
2024-09-26 16:31:23 +0600

    second commit

--- Code.c
+++ Code.c
@@ -8,3 +8,14 @@
     printf("Abs(%u) = %d\n",num,ans);
     
  }
+
+ #include<stdio.h>
+#include <stdlib.h>
+
+ int main(){
+    int num,ans;
+    scanf("%d",&num);
+    ans = abs(num);
+    printf("Abs(%u) = %d\n",num,ans);
+    
+ }
